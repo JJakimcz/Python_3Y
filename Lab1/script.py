aee1a0fecca9b4d6f571662b0a611ba9c602fe98
JJakimcz <88589226+JJakimcz@users.noreply.github.com>
2025-02-25 16:44:50 +0100

    Add files via upload

--- Lab1/script.py
+++ Lab1/script.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
+import math
+def zad13():
+    a = 1
+    b = 2
+    if a == b:
+        print("Tak")
+    else:
+        print("Nie")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def zad14():
+    a = 3
+    if a % 2 == 0:
+        print("Tak")
+    else:
+        print("Nie")
+
+def zad15():
+    a = 1
+    print(type(a))
+
+def zad16():
+    a = 1
+    b = 2
+
+    if a > b:
+        print(a)
+    else:
+        print(b)
+
+def zad17():
+    a = 9
+    b = 7
+    c = 3
+    d = 0
+    for x in [a, b, c]:
+        if x > d:
+            d = x
+    print(d)
+
+def zad18():
+    sigma = [10, 2, 1]
+    sigma.sort()
+    print(sigma)
+
+def zad19():
+    a = 1
+    b = 2
+    x = -b / a
+    print(x)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def zad20():
+    a = 1
+    b = 2
+    c = -3
+    d = b*b - 4*a*c
+    if d < 0:
+        print("Brak rozwiazań")
+    elif d == 0:
+        print(-b/(2*a))
+    else:
+        print("X1:", (-b - math.sqrt(d)) / (2 * a), "X2:", (-b + math.sqrt(d)) / (2 * a))
 
+def zad21():
+    a = 213
+    b = str(a)
+    for x in b:
+        print(x)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+zad13()
+zad14()
+zad15()
+zad16()
+zad17()
+zad18()
+zad19()
+zad20()
+zad21()

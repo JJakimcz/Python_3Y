97875a6c63d0ebc47f7bc50fd855979bc0642fac
JJakimcz <88589226+JJakimcz@users.noreply.github.com>
2025-03-04 16:15:45 +0100

    Add files via upload

---
+++ lab2/script.py
@@ -0,0 +1,37 @@
+def prime_selector(numbers):
+    for x in numbers:
+        for i in range(2, x):
+            if x % i == 0:
+                break
+            else:
+                print(x)
+                break
+
+def round_numbers(numbers, threshold):
+    for x in numbers:
+        if x < threshold:
+            print(round())
+        else:
+            print(x)
+
+def longest_increasing_subsequence(sequence):
+    for num in sequence:
+        temp = num
+        if num > temp:
+            
+
+
+def is_balanced(expression):
+
+def transposition_cipher(text, key):
+
+def fibonacci_generator(n):
+
+def most_frequent_element(data):
+
+def readability_score(text):
+
+def unique_permutations(text):
+
+def group_words_by_lenght(words):
+
